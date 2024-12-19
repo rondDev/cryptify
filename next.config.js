f0c9d6fcbfa7de478bce54c0678e0103ff94a115
rondDev <contact@rond.cc>
2024-12-19 15:10:32 +0100

    fix: change next config to allow static exports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+
+    output: "export",
+
+    basePath: "/cryptify",
+
+    images: {
+        unoptomized: true,
+    },
 };
 
 module.exports = nextConfig;
